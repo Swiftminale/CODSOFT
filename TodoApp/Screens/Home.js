3c31e2f7d0bb4e71a3b5bfbb9d0abd6780585a32
Swiftminale <minalefetene@gmail.com>
2024-04-16 18:25:57 +0300

    Replaced Kotli app with rect-native

---
+++ TodoApp/Screens/Home.js
@@ -0,0 +1,276 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Image,
+  Text,
+  FlatList,
+  StyleSheet,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  Alert,
+  ScrollView,
+} from "react-native";
+import { firebase } from "../firebaseConfig";
+import { FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+const Home = () => {
+  const [todos, setTodos] = useState([]);
+  const todoRef = firebase.firestore().collection("todos");
+  const [addData, setAddData] = useState("");
+  const navigation = useNavigation();
+  const [completedTodos, setCompletedTodos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    todoRef.orderBy("createdAt", "desc").onSnapshot((querySnapshot) => {
+      const todos = [];
+      querySnapshot.forEach((doc) => {
+        const { heading } = doc.data();
+        todos.push({
+          id: doc.id,
+          heading,
+        });
+      });
+      setTodos(todos);
+    });
+  }, []);
+
+  useEffect(() => {
+    setCompletedTodos(todos.filter((todo) => todo.completed));
+  }, [todos]);
+
+  const deleteTodo = (todo) => {
+    Alert.alert(
+      "Confirm Deletion",
+      "Are you sure you want to delete this todo?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: () => {
+            todoRef
+              .doc(todo.id)
+              .delete()
+              .then(() => {
+                alert("Deleted successfully");
+              })
+              .catch((error) => {
+                alert(error);
+              });
+          },
+          style: "destructive",
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const toggleCompletion = (todo) => {
+    const updatedTodos = todos.map((item) => {
+      if (item.id === todo.id) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      }
+      return item;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.heading.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const addTodo = () => {
+    if (addData && addData.length > 0) {
+      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+      const data = {
+        heading: addData,
+        createdAt: timestamp,
+      };
+      todoRef
+        .add(data)
+        .then(() => {
+          setAddData("");
+          Keyboard.dismiss();
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <Image
+          source={require("../assets/Img/list-amico.png")}
+          style={styles.image}
+        />
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search todos..."
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setSearchQuery(text)}
+            value={searchQuery}
+          />
+        </View>
+
+        <View style={styles.formContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Add new todo"
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(heading) => setAddData(heading)}
+            value={addData}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
+          />
+          <TouchableOpacity style={styles.button} onPress={addTodo}>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList
+          data={filteredTodos}
+          numColumns={1}
+          renderItem={({ item }) => (
+            <View>
+              <Pressable
+                style={styles.container}
+                onPress={() => navigation.navigate("Detail", { item })}
+              >
+                <TouchableOpacity onPress={() => toggleCompletion(item)}>
+                  <FontAwesome
+                    name={item.completed ? "close" : "circle-o"}
+                    color={item.completed ? "red" : "green"}
+                    style={styles.radioButton}
+                  />
+                </TouchableOpacity>
+                <View style={styles.innerContainer}>
+                  <Text
+                    style={
+                      item.completed
+                        ? [styles.itemHeading, styles.completedText]
+                        : styles.itemHeading
+                    }
+                  >
+                    {item.heading[0].toUpperCase() + item.heading.slice(1)}
+                  </Text>
+                </View>
+                <TouchableOpacity onPress={() => deleteTodo(item)}>
+                  <FontAwesome
+                    name="trash-o"
+                    color="red"
+                    style={styles.todoIcon}
+                  />
+                </TouchableOpacity>
+              </Pressable>
+              <Pressable></Pressable>
+            </View>
+          )}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e5e5e5",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
+    borderRadius: 15,
+    margin: 5,
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  radioButton: {
+    fontSize: 35,
+    marginRight: 0,
+  },
+  innerContainer: {
+    alignItems: "baseline",
+    flexDirection: "column",
+    marginLeft: 5,
+  },
+  itemHeading: {
+    fontWeight: "bold",
+    fontSize: 12,
+    marginRight: 12,
+  },
+  formContainer: {
+    flexDirection: "row",
+    height: 80,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 0,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
+  },
+  button: {
+    height: 47,
+    borderRadius: 5,
+    backgroundColor: "#788eec",
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+
+  todoIcon: {
+    marginTop: 5,
+    fontSize: 25,
+    marginLeft: 14,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+    marginHorizontal: 35,
+    resizeMode: "repeat",
+    opacity: 0.5,
+  },
+  todo: {
+    fontSize: 44,
+    textAlign: "center",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginBottom: 10,
+    position: "sticky",
+    top: 0,
+    zIndex: 1,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: "white",
+    paddingLeft: 10,
+    marginRight: 10,
+  },
+});
+
+export default Home;
---
+++ TodoApp/node_modules/@gorhom/bottom-sheet/README.md
@@ -0,0 +1,85 @@
+# React Native Bottom Sheet
+
+[![Reanimated v2 version](https://img.shields.io/github/package-json/v/gorhom/react-native-bottom-sheet/master?label=Reanimated%20v2&style=flat-square)](https://www.npmjs.com/package/@gorhom/bottom-sheet) [![Reanimated v1 version](https://img.shields.io/github/package-json/v/gorhom/react-native-bottom-sheet/v2?label=Reanimated%20v1&style=flat-square)](https://www.npmjs.com/package/@gorhom/bottom-sheet) [![npm](https://img.shields.io/npm/l/@gorhom/bottom-sheet?style=flat-square)](https://www.npmjs.com/package/@gorhom/bottom-sheet) [![npm](https://img.shields.io/badge/types-included-blue?style=flat-square)](https://www.npmjs.com/package/@gorhom/bottom-sheet) [![runs with expo](https://img.shields.io/badge/Runs%20with%20Expo-4630EB.svg?style=flat-square&logo=EXPO&labelColor=f3f3f3&logoColor=000)](https://expo.io/)<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-2-orange.svg?style=flat-square)](#contributors-)
+
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+
+A performant interactive bottom sheet with fully configurable options 🚀
+
+![React Native Bottom Sheet](./preview.gif)
+
+---
+
+## Features
+
+- Modal presentation view, [Bottom Sheet Modal](https://ui.gorhom.dev/components/bottom-sheet/modal).
+- Smooth gesture interactions & snapping animations.
+- Seamless [keyboard handling](https://ui.gorhom.dev/components/bottom-sheet/keyboard-handling) for iOS & Android.
+- Support [pull to refresh](https://ui.gorhom.dev/components/bottom-sheet/pull-to-refresh) for scrollables.
+- Support [FlatList](https://ui.gorhom.dev/components/bottom-sheet/components/bottomsheetflatlist), [SectionList](https://ui.gorhom.dev/components/bottom-sheet/components/bottomsheetsectionlist), [ScrollView](https://ui.gorhom.dev/components/bottom-sheet/components/bottomsheetscrollview) & [View](https://ui.gorhom.dev/components/bottom-sheet/components/bottomsheetview) scrolling interactions.
+- Support [React Navigation integration](https://ui.gorhom.dev/components/bottom-sheet/react-navigation-integration).
+- Compatible with `Reanimated` v1 & v2.
+- Compatible with `Expo`.
+- Accessibility support.
+- Written in `TypeScript`.
+- [Read more](https://ui.gorhom.dev/components/bottom-sheet).
+
+## Getting Started
+
+Check out [the documentation website](https://ui.gorhom.dev/components/bottom-sheet).
+
+## Versioning
+
+This library been written in 2 versions of `Reanimated`, and kept both implementation in 2 separate branches:
+
+- **`v2`** | [branch](https://github.com/gorhom/react-native-bottom-sheet/tree/v2) | [changelog](https://github.com/gorhom/react-native-bottom-sheet/blob/v2/CHANGELOG.md) : written with `Reanimated v1` & compatible with `Reanimated v2`.
+
+- **`v4`** | [branch](https://github.com/gorhom/react-native-bottom-sheet/tree/master) | [changelog](https://github.com/gorhom/react-native-bottom-sheet/blob/master/CHANGELOG.md) : written with `Reanimated v2`.
+
+> I highly recommend all `v3` users to upgrade to `v4` which provides more stability and all latest features.
+
+## Author
+
+- [Mo Gorhom](https://gorhom.dev/)
+
+## Contributors
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+> These people have helped to improve the library, but **DO NOT** maintain it.
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://react-native-training.eu"><img src="https://avatars.githubusercontent.com/u/1566403?v=4?s=76" width="76px;" alt=""/><br /><sub><b>Vojtech Novak</b></sub></a><br /><a href="https://github.com/gorhom/react-native-bottom-sheet/commits?author=vonovak" title="Code">💻</a></td>
+    <td align="center"><a href="https://github.com/kickbk"><img src="https://avatars.githubusercontent.com/u/31323376?v=4?s=76" width="76px;" alt=""/><br /><sub><b>kickbk</b></sub></a><br /><a href="https://github.com/gorhom/react-native-bottom-sheet/issues?q=author%3Akickbk" title="Bug reports">🐛</a> <a href="https://github.com/gorhom/react-native-bottom-sheet/commits?author=kickbk" title="Tests">⚠️</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## Sponsor & Support
+
+To keep this library maintained and up-to-date please consider [sponsoring it on GitHub](https://github.com/sponsors/gorhom). Or if you are looking for a private support or help in customizing the experience, then reach out to me on Twitter [@gorhom](https://twitter.com/gorhom).
+
+## License
+
+[MIT](./LICENSE)
+
+---
+
+<p align="center">
+  <a href="https://gorhom.dev/#gh-light-mode-only" target="_blank">
+    <img height="18" alt="Mo Gorhom" src="./mogorhom-light.png">
+  </a>
+  <a href="https://gorhom.dev/#gh-dark-mode-only" target="_blank">
+    <img height="18" alt="Mo Gorhom" src="./mogorhom-dark.png">
+  </a>
+</p>
+s
